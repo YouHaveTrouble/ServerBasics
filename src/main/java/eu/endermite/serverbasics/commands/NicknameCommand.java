af72e8d4d992e7c56dc5aa5b2bba1958b0683d7b
YouHaveTrouble <garrenpolska@gmail.com>
2020-11-20 01:50:42 +0100

    offline player support for /nick

--- src/main/java/eu/endermite/serverbasics/commands/NicknameCommand.java
+++ src/main/java/eu/endermite/serverbasics/commands/NicknameCommand.java
@@ -4,10 +4,13 @@ import cloud.commandframework.annotations.Argument;
 import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
 import cloud.commandframework.annotations.CommandPermission;
+import cloud.commandframework.bukkit.arguments.selector.SinglePlayerSelector;
 import eu.endermite.serverbasics.ServerBasics;
+import eu.endermite.serverbasics.messages.MessageParser;
 import eu.endermite.serverbasics.storage.PlayerDatabase;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
@@ -20,14 +23,46 @@ public class NicknameCommand {
     @CommandDescription("Set your nickname")
     @CommandPermission("serverbasics.command.nick.reset")
     private void commandNickReset(
-            final Player player,
-            final @Argument(value = "player") OfflinePlayer target
+            final CommandSender sender,
+            final @Argument(value = "player") SinglePlayerSelector player
     ) {
+
+        if (!player.hasAny()) {
+            try {
+                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(player.getSelector());
+                if (!PlayerDatabase.playerExists(offlinePlayer.getUniqueId())) {
+                    sendHaventPlayedError(sender);
+                    return;
+                }
+
+                PlayerDatabase.saveSingleOption(offlinePlayer.getUniqueId(), "displayname", offlinePlayer.getName());
+
+                Component message = Component.translatable(
+                        "commands.scoreboard.objectives.modify.displayname",
+                        NamedTextColor.WHITE,
+                        Component.text(offlinePlayer.getName()),
+                        Component.text(ChatColor.translateAlternateColorCodes('&', offlinePlayer.getName()))
+                );
+                ServerBasics.getCommandManager().bukkitAudiences.sender(sender).sendMessage(message);
+                return;
+            } catch (Exception e) {
+                sendHaventPlayedError(sender);
+            }
+        }
+
+        Player target = player.getPlayer();
+
         String nick = target.getName();
 
-        PlayerDatabase.saveSingleOption(player.getUniqueId(), "displayname", nick);
-        if (!target.isOnline())
-            return;
+        Component message = Component.translatable(
+                "commands.scoreboard.objectives.modify.displayname",
+                NamedTextColor.WHITE,
+                Component.text(target.getName()),
+                Component.text(ChatColor.translateAlternateColorCodes('&', nick))
+        );
+        ServerBasics.getCommandManager().bukkitAudiences.sender(sender).sendMessage(message);
+
+        PlayerDatabase.saveSingleOption(target.getUniqueId(), "displayname", nick);
 
         Player onlineTarget = target.getPlayer();
         ServerBasics.getBasicPlayers().getBasicPlayer(target.getUniqueId()).setDisplayName(nick);
@@ -40,11 +75,35 @@ public class NicknameCommand {
     @CommandPermission("serverbasics.command.nick")
     private void commandNickOther(
             final CommandSender sender,
-            final @Argument(value = "player") OfflinePlayer target,
+            final @Argument(value = "player") SinglePlayerSelector player,
             final @Argument(value = "nickname") String[] newNick
     ) {
         String nick = String.join(" ", newNick);
 
+        if (!player.hasAny()) {
+            try {
+                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(player.getSelector());
+                if (!PlayerDatabase.playerExists(offlinePlayer.getUniqueId())) {
+                    sendHaventPlayedError(sender);
+                    return;
+                }
+
+                PlayerDatabase.saveSingleOption(offlinePlayer.getUniqueId(), "displayname", nick);
+
+                Component message = Component.translatable(
+                        "commands.scoreboard.objectives.modify.displayname",
+                        NamedTextColor.WHITE,
+                        Component.text(offlinePlayer.getName()),
+                        Component.text(ChatColor.translateAlternateColorCodes('&', nick))
+                );
+                ServerBasics.getCommandManager().bukkitAudiences.sender(sender).sendMessage(message);
+                return;
+            } catch (Exception e) {
+                sendHaventPlayedError(sender);
+            }
+        }
+
+        Player target = player.getPlayer();
 
         PlayerDatabase.saveSingleOption(target.getUniqueId(), "displayname", nick);
 
@@ -62,6 +121,17 @@ public class NicknameCommand {
         Player onlineTarget = target.getPlayer();
         ServerBasics.getBasicPlayers().getBasicPlayer(target.getUniqueId()).setDisplayName(nick);
         onlineTarget.setDisplayName(ChatColor.translateAlternateColorCodes('&', nick));
+    }
+
+    private void sendHaventPlayedError(CommandSender sender) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            String msg = ServerBasics.getLang(player.getLocale()).HAVENT_PLAYED;
+            MessageParser.sendMessage(player, msg);
+        } else {
+            String msg = ServerBasics.getLang(ServerBasics.getConfigCache().DEFAULT_LANG).HAVENT_PLAYED;
+            MessageParser.sendMessage(sender, msg);
+        }
     }
 
 }
